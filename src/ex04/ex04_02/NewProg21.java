c7d2970508b3e3ed0f3bc137a9a9a1806ef4b674
wm <>
2012-12-09 19:19:45 +0100

    Returned single ball shot

--- src/ex04/ex04_02/NewProg21.java
+++ src/ex04/ex04_02/NewProg21.java
@@ -9,27 +9,49 @@ import javax.swing.*;
 /**
  * Klasa obiektu odpowiedzalnego za zmiane współrzednych strzału.
  */
-class NewStrzal extends Thread {
+//class NewStrzal extends Thread {
+//
+//    int x_strzal, y_strzal, h_strzal = 10;
+//    NewProg21 okno;
+//    int predkosc = 15;
+//
+//    NewStrzal(NewProg21 okno) {
+//        this.okno = okno;
+//    }
+//
+//    public void run() {
+//        while (y_strzal > 0) {
+//            y_strzal -= predkosc;
+//            try {
+//                Thread.sleep(40);
+//            } catch (Exception e) {
+//            };
+//        }
+//        okno.list.remove(this);
+//    }
+//}
 
-    int x_strzal, y_strzal, h_strzal = 10;
+class Strzal extends Thread {
+
     NewProg21 okno;
-    int predkosc = 15;
+    int predkosc = 45;
 
-    NewStrzal(NewProg21 okno) {
+    Strzal(NewProg21 okno) {
         this.okno = okno;
     }
 
     public void run() {
-        while (y_strzal > 0) {
-            y_strzal -= predkosc;
+        while (okno.y_strzal > 0) {
+            okno.y_strzal -= predkosc;
             try {
-                Thread.sleep(40);
+                Thread.sleep(50);
             } catch (Exception e) {
             };
         }
-        okno.list.remove(this);
+        okno.jestStrzal = false;
     }
 }
+
 
 class MyListener extends MouseAdapter {
 
@@ -46,22 +68,24 @@ class MyListener extends MouseAdapter {
     }
 
     public void mouseMoved(MouseEvent e) {
-        if (e.getX() < okno.w - okno.szerokosc) {
-            okno.prevX = okno.x;
-            okno.x = e.getX();
-            okno.isMove = true;
+        if (e.getX() < okno.windowWidth - okno.pWidth) {
+            okno.prevOneX = okno.pOneX;
+            okno.pOneX = e.getX();
+            okno.isMoveOne = true;
         }
     }
 }
 
 public class NewProg21 extends JFrame implements KeyListener, Runnable {
 
-    int w = 500, h = 500;
-    int x = 200, y = 400, x2 = 200, y2 = 100, szerokosc = 50, wysokosc = 10;
-    int prevX, prevX2 = 0;
-    boolean isMove, isMove2 = true;
-    int predkosc = 5;
-    List<NewStrzal> list = new LinkedList<NewStrzal>();
+    int windowWidth = 500, windowHeight = 600;
+    int pOneX = 200, pOneY = 500, pTwoX = 200, pTwoY = 100, pWidth = 50, pHeight = 10;
+    int prevOneX, prevTwoX = 0;
+    boolean isMoveOne, isMoveTwo = true;
+    int pSpeed = 5;
+    static int x_strzal, y_strzal, h_strzal = 10;
+    static boolean jestStrzal = false;
+    // List<NewStrzal> list = new LinkedList<NewStrzal>();
 
     public static void main(String[] args) {
         NewProg21 okno = new NewProg21("Super Gra -- :)...");
@@ -84,59 +108,51 @@ public class NewProg21 extends JFrame implements KeyListener, Runnable {
     void init() {
         Container cp = getContentPane();
         setBackground(Color.yellow);
-        setSize(w, h);
+        setSize(windowWidth, windowHeight);
         setResizable(false);
     }
 
     /**
-     * Inicjalizacja strzalu - uruchomienie watka zmieniajacego wspolrzedne
-     * strzalu.
+     * Inicjalizacja strzalu - uruchomienie watka zmieniajacego wspolrzedne strzalu.
      */
     void strzal() {
-
-        NewStrzal strzal = new NewStrzal(this);
-        strzal.h_strzal = 10;
-        strzal.x_strzal = x + szerokosc / 2 - strzal.h_strzal / 2;
-        strzal.y_strzal = y - strzal.h_strzal;
-
-        list.add(strzal);
-        strzal.start();
+        if (!jestStrzal) {
+            x_strzal = pOneX + pWidth / 2 - h_strzal / 2;
+            y_strzal = pOneY - h_strzal;
+            jestStrzal = true;
+            new Strzal(this).start();
+        }
     }
 
-//    void przesun(int kierunek) {
-//        x += (kierunek * predkosc);
-//    }
-    void przesun2(int kierunek) {
-        if (kierunek == 1 && x2 < w - szerokosc) {
-            prevX2 = x2;
-            x2 += (kierunek * predkosc);
-            isMove2 = true;            
-            System.out.println("prawo: " + x2);
-            
+
+    void moveP2(int pDirection) {
+        if (pDirection == 1 && pTwoX < windowWidth - pWidth) {
+            prevTwoX = pTwoX;
+            pTwoX += (pDirection * pSpeed);
+            isMoveTwo = true;            
         } else
-            if (kierunek == -1 && x2 > 0) {
-            prevX2 = x2;
-            x2 += (kierunek * predkosc);
-            isMove2 = true;
-            System.out.println("lewo: " + x2);
+            if (pDirection == -1 && pTwoX > 0) {
+            prevTwoX = pTwoX;
+            pTwoX += (pDirection * pSpeed);
+            isMoveTwo = true;            
         }
     }
 
     public void paint(Graphics g) { // metoda odrysowujaca ekran
-        if (isMove2) {
-            g.clearRect(prevX2, y2, szerokosc, wysokosc);
-            isMove2 = false;
+        if (isMoveTwo) {
+            g.clearRect(prevTwoX, pTwoY, pWidth, pHeight);
+            isMoveTwo = false;
         }
-        if (isMove) {
-            g.clearRect(prevX, y, szerokosc, wysokosc);
-            isMove2 = false;
+        if (isMoveOne) {
+            g.clearRect(prevOneX, pOneY, pWidth, pHeight);
+            isMoveOne = false;
         }
-        g.fillRect(x2, y2, szerokosc, wysokosc);
-        g.fillRect(x, y, szerokosc, wysokosc);
+        g.fillRect(pTwoX, pTwoY, pWidth, pHeight);
+        g.fillRect(pOneX, pOneY, pWidth, pHeight);
 
 
-        for (NewStrzal strzal : list) {
-            g.fillOval(strzal.x_strzal, strzal.y_strzal, strzal.h_strzal, strzal.h_strzal);
+        if (jestStrzal) {
+            g.fillOval(x_strzal, y_strzal, h_strzal, h_strzal);
         }
 
 
@@ -164,10 +180,10 @@ public class NewProg21 extends JFrame implements KeyListener, Runnable {
     public void keyPressed(KeyEvent e) { // reaguje jedynie na przycisniecie klawisza
         switch (e.getKeyCode()) {
             case KeyEvent.VK_LEFT:
-                przesun2(-1);
+                moveP2(-1);
                 break;
             case KeyEvent.VK_RIGHT:
-                przesun2(1);
+                moveP2(1);
                 break;
             case KeyEvent.VK_SPACE:
                 strzal();
